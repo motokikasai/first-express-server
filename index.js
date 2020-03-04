9b229d78c1cd9e8241ed2c5ab6127f560277e8c6
motokikasai <kmomoky@hotmail.com>
2020-03-04 10:40:16 +0100

    Add template to POST method and Joi schema

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const students = [
     lastname: "Lewis",
     id: 2,
     age: 18,
-    class: "FBW101",
+    class: "FBW102",
     location: "Stuttgart"
   },
   {
@@ -26,7 +26,7 @@ const students = [
     lastname: "Poe",
     id: 3,
     age: 33,
-    class: "FBW101",
+    class: "FBW103",
     location: "Dresden"
   },
   {
@@ -34,7 +34,7 @@ const students = [
     lastname: "Arthur",
     id: 4,
     age: 25,
-    class: "FBW101",
+    class: "FBW104",
     location: "Hanover"
   },
   {
@@ -42,7 +42,7 @@ const students = [
     lastname: "Bond",
     id: 5,
     age: 23,
-    class: "FBW101",
+    class: "FBW105",
     location: "Berlin"
   }
 ];
@@ -58,9 +58,11 @@ const schema = Joi.object({
     .min(3)
     .max(30)
     .required(),
-  age: Joi.string()
+  age: Joi.number()
     .min(18)
-    .required()
+    .required(),
+  class: Joi.string(),
+  location: Joi.string()
 });
 
 /** -----------------------------------------------------
@@ -96,7 +98,21 @@ infoGetter("location");
  * -----------------------------------------------------*/
 
 app.post("/api/students", (req, res) => {
-  //  ...
+  const { error, value } = schema.validate(req.body);
+  if (error) res.status(404).send(error.details[0].message);
+
+  // Template of new student data to be added...
+  const student = {
+    name: req.body.name,
+    lastname: req.body.lastname,
+    id: students.length + 1,
+    age: req.body.age,
+    class: req.body.class,
+    location: req.body.location
+  };
+
+  students.push(student);
+  res.send(student);
 });
 
 /** -----------------------------------------------------
